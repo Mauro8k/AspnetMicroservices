140f9c2b9fa062a72b5c5a1c5ce18ed2f9d1c070
Mauro8k <mauro8k@gmail.com>
2023-01-29 12:48:54 -0500

    add repository part

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalagContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var client = new MongoClient(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var dataBase = client.GetDatabase(_configuration.GetValue<string>("DatabaseSettings:DataBaseName"));
+
+            Products = dataBase.GetCollection<Product>(_configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products  {get;}
+    }
+}
